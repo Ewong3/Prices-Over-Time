f6f5a171cbf4ab299d9b1da6f7189dd61a168d62
Ewong3 <Ewong3@users.noreply.github.com>
2018-08-05 19:01:23 -0600

    Integrated Handlebars for rendering
    - Integrated Handlebars for rendering

--- App/prices-over-time-app.js
+++ App/prices-over-time-app.js
@@ -1,15 +1,31 @@
 var express = require('express');
 var parser = require('body-parser');
 var path = require("path");
+var engines = require('consolidate');
+require('handlebars');
 
 var app = express();
+
+app.set('views', __dirname + '/html');
+app.engine('html', engines.handlebars);
+app.set('view engine', 'html');
 app.use(express.static( __dirname + '' ));
+
 app.use(parser.json());
 app.use(parser.urlencoded({extended : true}));
 
+app.set('views', path.join(__dirname, 'html'));
+
 app.get('/Products', (request, response) => {
-  response.sendFile(path.join(__dirname+'/html/products.html'));
+  response.render('product.html');
 });
+
+app.get('/Product/:id', (request, response) => {
+  var id = request.params.id;
+
+  response.render('product.html', { id : id });
+});
+
 
 https://stackoverflow.com/questions/19254029/angularjs-http-post-does-not-send-data
 var server = app.listen(5001, function () {
